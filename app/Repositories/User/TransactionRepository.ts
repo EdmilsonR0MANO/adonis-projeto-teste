db8a3fbcf97235ac6f156c17532df02b5f9d74fa
Edmilson Romano <edmilsonromano14@gmail.com>
2023-10-23 07:23:47 -0300

    fix: transactionRepository

--- app/Repositories/User/TransactionRepository.ts
+++ app/Repositories/User/TransactionRepository.ts
@@ -4,8 +4,8 @@ import TransactionDto from 'app/Dtos/TransactionsDto'
 
 
 export default class TransactionRepository{
-    public async create(transactionDto: TransactionDto): Promise<Transaction> {
-        return await Transaction.create(transactionDto)
+    public async create(transaction: TransactionDto): Promise<Transaction> {
+        return await Transaction.create(transaction)
         
     }
 }
